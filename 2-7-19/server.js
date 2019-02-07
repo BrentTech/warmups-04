d86adf9461cd971b20394bcb57f941431b63bd7e
Brent Woodward <Brent.Woodward.1@gmail.com>
2019-02-07 09:29:53 -0800

    swapi people fetched!

---
+++ 2-7-19/server.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const superagent = require('superagent');
+const url = 'https://swapi.co/api/people'
+
+
+let fetchPeopleWithPromises = () => {
+  return superagent.get(url)
+    .then( res => {
+      return res.body.results.map((person) => {
+        return superagent.get(person.url);
+      });
+    })
+    .then( peoplePromises => {
+      return Promise.all(peoplePromises)
+        .then( people => {
+          let names = [];
+          for( let data of people) {
+            names.push(data.body.name);
+          }
+          return names;
+        });
+    })
+    .catch( console.error );
+};
+
+let fetchPeopleWithAsync = async () => {
+  try {
+    let peopleSet = await superagent.get(url);
+    let people = (peopleSet.body && peopleSet.body.results) || [];
+    let peopleRequests = people.map ( person => {
+      return superagent.get(person.url);
+    });
+    let swapiNames = await Promise.all(peopleRequests)
+      .then( people => {
+        let names = [];
+        for(let data of people) {
+          names.push(data.body.name);
+        }
+        return names;
+      });
+    return swapiNames;
+  }
+  catch( err ) { console.error(err); }
+};
+
+fetchPeopleWithPromises()
+  .then( people => console.log('Promise People', people));
+
+fetchPeopleWithAsync()
+  .then( names => console.log('Async Names', names));
+  
